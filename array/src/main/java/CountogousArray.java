75c88e205b0e5689eb682b2f65ce7c9b1f1b4747
xeadmin <sunny.makhija84@gmail.com>
2022-03-24 18:27:31 +0530

    pushed

--- array/src/main/java/CountogousArray.java
+++ array/src/main/java/CountogousArray.java
@@ -1,17 +1,61 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class CountogousArray {
     public static void main(String[] a)
     {
-        int[] arr={1,2,3,4,7,8};
-        System.out.println(countContinousArray(arr));
+        int[] arr={1,1,2,3,4,7,8};
+        System.out.println("hell"+countContinousArray(arr));
+
+        System.out.println(countContinousOptimizeArray(arr));
+    }
+
+    private static int countContinousOptimizeArray(int[] arr) {
+        int count=0;
+        HashSet<Integer> st= new HashSet<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            st.add(arr[i]);
+
+        }
+        int max=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            count=0;
+            if(!st.contains(arr[i]-1))
+            {
+                int j= arr[i];
+                while(st.contains(j))
+                {
+                    j++;
+                    count++;
+                }
+
+
+            }
+            if(count>max)
+                max=count;
+        }
+        return max;
     }
 
     private static int countContinousArray(int[] arr) {
-        int[] temp= new int[arr.length];
+        List<Integer> arLst= new ArrayList<>();
+
         int count=0;
         int ans=0;
-        for(int i=0;i<arr.length;i++)
+        arLst.add(arr[0]);
+        for(int i=1;i<arr.length;i++)
         {
-            if(i>0 && arr[i]== arr[i-1]+1)
+            if(arr[i] != arr[i-1])
+            {
+                arLst.add(arr[i]);
+            }
+        }
+        for(int i=0;i<arLst.size();i++)
+        {
+            if(i>0 && arLst.get(i)== arLst.get(i-1)+1)
             {
                 count++;
             }else
